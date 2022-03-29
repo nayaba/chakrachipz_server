6b13599dc36c5ee31577250970d5bced03d5caaf
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-03-29 15:12:27 -0400

    Create db connection file for mongodb

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/chakrachipzDB')
+  .then(() => {
+    console.log('Successfully connected to MongoDB. 🧁')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db
